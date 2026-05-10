26c78aeb2da57dad7f6253ae5f48a1bb453293a0
erimow <erik.morris922@gmail.com>
2026-05-10 00:13:03 -0700

    voice message concatenating. still WIP

--- src/c/Memos.c
+++ src/c/Memos.c
@@ -1,5 +1,6 @@
 #include <pebble.h>
 #include <stdint.h>
+#include <string.h>
 
 /* ---------- AppMessage keys ---------- */
 enum {
@@ -14,6 +15,11 @@ static Window *s_main_window;
 static TextLayer *s_text_layer;
 static MenuLayer *s_menu_layer;
 static ActionBarLayer *s_actionbar_layer;
+static ScrollLayer *s_scroll_layer;
+
+/* ---------- Images ---------- */
+static GBitmap *send_image;
+static GBitmap *mic_image;
 
 /* ---------- Dictation ---------- */
 static DictationSession *s_dictation;
@@ -23,11 +29,15 @@ static AppTimer *s_status_timer;
 
 /* ---------- Values ----------- */
 static uint16_t s_num_menu_rows = 0;
+static char s_message[1024] = "";
 
 /* ---------- Forward declarations ---------- */
 static void start_dictation(void);
 static void show_status(const char *msg, bool success);
 static void clear_status(void *data);
+static void click_config_provider(void *context);
+static void scroll_select_callback(ClickRecognizerRef ref, void *context);
+static void action_bar_click_config_provider(void *context);
 
 /* =================================================================== */
 /*                           CALLBACKS                                 */
@@ -48,6 +58,7 @@ static void menu_draw_row_callback(GContext *ctx, const Layer *cell_layer,
   }
 }
 
+
 /* =================================================================== */
 /*                               UI                                    */
 /* =================================================================== */
@@ -57,14 +68,22 @@ static void main_window_load(Window *window) {
   GRect bounds = layer_get_bounds(window_layer);
 
   s_text_layer =
-      text_layer_create(GRect(5, 20, bounds.size.w - 10, bounds.size.h - 40));
+      text_layer_create(GRect(1, 0, bounds.size.w - 1, bounds.size.h*4));
 
   text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
   text_layer_set_font(s_text_layer,
                       fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
   text_layer_set_text(s_text_layer, "Press Select\nto Dictate");
+  
+  s_scroll_layer = scroll_layer_create(bounds);
 
-  layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
+  scroll_layer_set_callbacks(s_scroll_layer, (ScrollLayerCallbacks){.click_config_provider = click_config_provider});
+  scroll_layer_set_content_size(s_scroll_layer, (GSize){bounds.size.w,bounds.size.h*4});
+
+  scroll_layer_add_child(s_scroll_layer, text_layer_get_layer(s_text_layer));
+  layer_add_child(window_layer, scroll_layer_get_layer(s_scroll_layer));
+  scroll_layer_set_click_config_onto_window(s_scroll_layer, s_main_window);
+
 
   // s_menu_layer = menu_layer_create(bounds);
   // menu_layer_set_click_config_onto_window(s_menu_layer, window);
@@ -75,11 +94,15 @@ static void main_window_load(Window *window) {
   //                          .select_long_click = menu_select_long_callback,
   //                          .select_click = menu_select_callback});
   // layer_add_child(window_layer, menu_layer_get_layer(s_menu_layer));
+  mic_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_DICTATE);
+  send_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_SEND);
 
-  // s_actionbar_layer = action_bar_layer_create();
+  s_actionbar_layer = action_bar_layer_create();
+  action_bar_layer_set_icon_animated(s_actionbar_layer, BUTTON_ID_SELECT, mic_image, true);
+  action_bar_layer_set_icon_animated(s_actionbar_layer, BUTTON_ID_DOWN, send_image, true);
   //
-  // layer_add_child(window_layer,
-  // action_bar_layer_get_layer(s_actionbar_layer));
+  //  layer_add_child(window_layer,
+  //  action_bar_layer_get_layer(s_actionbar_layer));
 }
 
 static void main_window_unload(Window *window) {
@@ -91,7 +114,10 @@ static void main_window_unload(Window *window) {
 /* =================================================================== */
 
 static void clear_status(void *data) {
-  text_layer_set_text(s_text_layer, "Press Select\nto Dictate");
+  if (s_message[0]=='\0')
+    text_layer_set_text(s_text_layer, "Press Select\nto Dictate");
+  else
+    text_layer_set_text(s_text_layer, s_message);
   s_status_timer = NULL;
 }
 
@@ -110,6 +136,25 @@ static void show_status(const char *msg, bool success) {
 
   s_status_timer = app_timer_register(2000, clear_status, NULL);
 }
+/* =================================================================== */
+/*                          SENDING                                    */
+/* =================================================================== */
+static void send(char *message){
+  
+  /* ---- Send transcription to JS (JS handles chunking) ---- */
+  DictionaryIterator *iter;
+  if (app_message_outbox_begin(&iter) != APP_MSG_OK) {
+    show_status("Send Failed", false);
+    return;
+  }
+
+  dict_write_cstring(iter, KEY_MEMO_CHUNK, message);
+  dict_write_uint8(iter, KEY_MEMO_DONE, 1);
+
+  if (app_message_outbox_send() != APP_MSG_OK) {
+    show_status("Send Failed", false);
+  }
+}
 
 /* =================================================================== */
 /*                          DICTATION                                   */
@@ -122,22 +167,19 @@ static void dictation_callback(DictationSession *session,
     show_status("Dictation Failed", false);
     return;
   }
-
-  text_layer_set_text(s_text_layer, "Sending…");
-
-  /* ---- Send transcription to JS (JS handles chunking) ---- */
-  DictionaryIterator *iter;
-  if (app_message_outbox_begin(&iter) != APP_MSG_OK) {
-    show_status("Send Failed", false);
+  // if dictation does not fail
+  if ((strlen(s_message)+strlen(transcription))>1024){
+    show_status("Buffer overload", false);
     return;
   }
 
-  dict_write_cstring(iter, KEY_MEMO_CHUNK, transcription);
-  dict_write_uint8(iter, KEY_MEMO_DONE, 1);
+  if (s_message[0]!='\0')
+    strcat(s_message, " "); //add a space if not the first dictation
+  strcat(s_message, transcription);
+  // strcat_s(s_message, transcription, 1024);
+  // strncpy(s_message, transcription,1024);
+  text_layer_set_text(s_text_layer, s_message);
 
-  if (app_message_outbox_send() != APP_MSG_OK) {
-    show_status("Send Failed", false);
-  }
 }
 
 static void start_dictation(void) {
@@ -157,6 +199,7 @@ static void start_dictation(void) {
 static void inbox_received_handler(DictionaryIterator *iter, void *context) {
   if (dict_find(iter, KEY_MEMO_OK)) {
     show_status("Sent 👍", true);
+    s_message[0]='\0';
   } else if (dict_find(iter, KEY_MEMO_FAIL)) {
     show_status("Send Failed", false);
   }
@@ -170,31 +213,57 @@ static void inbox_dropped_handler(AppMessageResult reason, void *context) {
 /*                           INPUT                                      */
 /* =================================================================== */
 
-static void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index,
-                                 void *context) {
-  // enlarge note
-}
+// static void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index,
+//                                  void *context) {
+//   // enlarge note
+// }
 
-static void menu_select_long_callback(MenuLayer *menu_layer,
-                                      MenuIndex *cell_index, void *context) {
-  // do something on long press?
-}
+// static void menu_select_long_callback(MenuLayer *menu_layer,
+//                                       MenuIndex *cell_index, void *context) {
+//   // do something on long press?
+// }
 
-static void select_click_handler(ClickRecognizerRef ref, void *context) {
+static void action_select_click_handler(ClickRecognizerRef ref, void *context) {
   start_dictation();
+  action_bar_layer_remove_from_window(s_actionbar_layer);
+  scroll_layer_set_click_config_onto_window(s_scroll_layer, s_main_window);
 }
-// static void select_click_handler(ClickRecognizerRef ref, void *context) {
-//
-// }
-// static void select_click_handler(ClickRecognizerRef ref, void *context) {
-//
-// }
+static void action_up_click_handler(ClickRecognizerRef ref, void *context) {
+//???
+  action_bar_layer_remove_from_window(s_actionbar_layer);
+  scroll_layer_set_click_config_onto_window(s_scroll_layer, s_main_window);
+}
+static void action_down_click_handler(ClickRecognizerRef ref, void *context) {
+  send(s_message);
+  action_bar_layer_remove_from_window(s_actionbar_layer);
+  scroll_layer_set_click_config_onto_window(s_scroll_layer, s_main_window);
+}
+static void action_back_click_handler(ClickRecognizerRef ref, void *context) {
+  action_bar_layer_remove_from_window(s_actionbar_layer);
+  scroll_layer_set_click_config_onto_window(s_scroll_layer, s_main_window);
+}
+static void scroll_back_callback(ClickRecognizerRef ref, void *context){
+  window_stack_pop(true);
+}
+static void scroll_select_callback(ClickRecognizerRef ref, void *context){
+  // start_dictation();
+  action_bar_layer_add_to_window(s_actionbar_layer, s_main_window);
+  action_bar_layer_set_click_config_provider(s_actionbar_layer, action_bar_click_config_provider);
+}
 
 static void click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-  // window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
-  // window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+  window_single_click_subscribe(BUTTON_ID_SELECT, scroll_select_callback);
+  window_single_click_subscribe(BUTTON_ID_BACK, scroll_back_callback);
+//   window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
 }
+
+static void action_bar_click_config_provider(void *context){
+  window_single_click_subscribe(BUTTON_ID_BACK, action_back_click_handler);
+  window_single_click_subscribe(BUTTON_ID_UP, action_up_click_handler);
+  window_single_click_subscribe(BUTTON_ID_SELECT, action_select_click_handler);
+  window_single_click_subscribe(BUTTON_ID_DOWN, action_down_click_handler);
+}
+
 
 /* =================================================================== */
 /*                          APP LIFECYCLE                                */
@@ -221,21 +290,32 @@ static void init(void) {
 }
 
 static void deinit(void) {
+  if (mic_image){
+    gbitmap_destroy(mic_image);
+  }
+  if (send_image){
+    gbitmap_destroy(send_image);
+  }
+
   if (s_dictation) {
     dictation_session_destroy(s_dictation);
+  }
+
+  if (s_scroll_layer){
+    scroll_layer_destroy(s_scroll_layer);
   }
 
   if (s_status_timer) {
     app_timer_cancel(s_status_timer);
   }
 
-  // if (s_menu_layer) {
-  //   menu_layer_destroy(s_menu_layer);
-  // }
+  if (s_menu_layer) {
+    menu_layer_destroy(s_menu_layer);
+  }
 
-  // if (s_actionbar_layer) {
-  //   action_bar_layer_destroy(s_actionbar_layer);
-  // }
+  if (s_actionbar_layer) {
+    action_bar_layer_destroy(s_actionbar_layer);
+  }
 
   window_destroy(s_main_window);
 }
